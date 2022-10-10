b25fb9fa01558cfe1981d2ccc05fb034ecb9f1a3
AlexandrePoix <115186238+AlexandrePoix@users.noreply.github.com>
2022-10-10 14:18:09 +0200

    Optimisation en ligne et dégobage

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,17 +1,31 @@
 from fileinput import close
 import string
 import sys
-n = 0
+n = y = z = 0 
 e = ""
 t = ["*", "//", "+", "/", "%", "-", "**"]
 while e != "quit":
-    a = input ("Entrez un premier nombre : ")
-    b = input ("Entrez un second nombre : ")
-    c = input ("Quelle fonction voulez vous utiliser : ")
-    if ((isinstance(a, int) == "False") or ((isinstance(b,int)) == "False")) or (c in t) == "False":
-        print("Merci de respecter les consignes d'utilisation")
-    else: n = 1
+    while n == 0:
+        a = input ("Entrez un premier nombre : ")
+        if a.isnumeric() == False:
+            print ("Merci de remplir avec un nombre")
+        else: n = 1
+    while z == 0:
+        b = input ("Entrez un second nombre : ")
+        if b.isnumeric() == False:
+            print("Merci de remplir avec un nombre") 
+        else:
+            z = 1
+    while y == 0:
+        y = 0
+        c = input ("Quelle fonction voulez vous utiliser : ")
+        if (c in t) == False:
+            print("Merci de remplir avec un opérateur de calcul")
+        else: y = 1
     res = eval(a + c + b)
     print ("Le résultat de l'opération " + a,""  + c, "" + b, "est égal à : %s" %res)
+    n = 0
+    y = 0
+    z = 0
     e = input("Taper \"quit\" si vous voulez quitter le programme sinon appuyer sur une autre touche : ")
 else: sys.exit()
