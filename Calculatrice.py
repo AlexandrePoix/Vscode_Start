2d92e43fa0c44da095850c0edea9fe8ab3052bca
AlexandrePoix <115186238+AlexandrePoix@users.noreply.github.com>
2022-10-10 14:48:05 +0200

    Meilleur visibilité

--- Calculatrice.py
+++ Calculatrice.py
@@ -1,31 +1,37 @@
 from fileinput import close
 import string
 import sys
-n = y = z = 0 
+n = z = y = 0
 e = ""
 t = ["*", "//", "+", "/", "%", "-", "**"]
 while e != "quit":
     while n == 0:
+        print()
         a = input ("Entrez un premier nombre : ")
         if a.isnumeric() == False:
             print ("Merci de remplir avec un nombre")
         else: n = 1
     while z == 0:
+        print()
         b = input ("Entrez un second nombre : ")
         if b.isnumeric() == False:
             print("Merci de remplir avec un nombre") 
         else:
             z = 1
     while y == 0:
+        print()
         y = 0
         c = input ("Quelle fonction voulez vous utiliser : ")
         if (c in t) == False:
             print("Merci de remplir avec un opérateur de calcul")
         else: y = 1
     res = eval(a + c + b)
+    print()
     print ("Le résultat de l'opération " + a,""  + c, "" + b, "est égal à : %s" %res)
     n = 0
     y = 0
     z = 0
+    print()
     e = input("Taper \"quit\" si vous voulez quitter le programme sinon appuyer sur une autre touche : ")
+    print()
 else: sys.exit()
